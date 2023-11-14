d5392844a992b188415ac95f6a59555c74db5603
Pijus <pijus.petkevicius@bentley.com>
2023-11-14 18:00:33 +0200

    util functions

--- VQLS-QSVM-Implementation/Utils.py
+++ VQLS-QSVM-Implementation/Utils.py
@@ -6,8 +6,24 @@ def zGate() -> np.array:
 def identityGate() -> np.array:
     return np.eye(2)
 
+def letterToGate(letter:str) -> np.array:
+    if letter == "Z":
+        return zGate()
+    elif letter == "I":
+        return identityGate()
+    else:
+        return None
+
 def createMatrixFromParameters(coefs:list, gates:list) -> np.array:
-    matrix = np.zeros((len(gates[0]), len(gates[0])), dtype=complex)
+    matrix = np.zeros((2**len(gates[0]), 2**len(gates[0])), dtype=complex)
     for gate in gates:
-        combDate = 
-        matrix += coefs[gates.index(gate)] * gate
+        tempGate = None
+        gate = gate[::-1]
+        for letter in gate:
+            if tempGate is None:
+                tempGate = letterToGate(letter)
+            else:
+                tempGate = np.kron(tempGate, letterToGate(letter))
+        matrix += coefs[gates.index(gate)] * tempGate
+
+    return matrix
