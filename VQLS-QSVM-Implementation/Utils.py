90d56e7d6056b4508712fe2b6febfe912391bac6
Pijus <pijus.petkevicius@bentley.com>
2023-11-14 22:38:08 +0200

    transpilation

--- VQLS-QSVM-Implementation/Utils.py
+++ VQLS-QSVM-Implementation/Utils.py
@@ -3,6 +3,13 @@ import numpy as np
 def zGate() -> np.array:
     return np.array([[1, 0], [0, -1]])
 
+def xGate() -> np.array:
+    return np.array([[0, 1], [1, 0]])
+
+def yGate() -> np.array:
+    return np.array([[0, -1j], [1j, 0]])
+
+
 def identityGate() -> np.array:
     return np.eye(2)
 
@@ -11,6 +18,10 @@ def letterToGate(letter:str) -> np.array:
         return zGate()
     elif letter == "I":
         return identityGate()
+    elif letter == "X":
+        return xGate()
+    elif letter == "Y":
+        return yGate()
     else:
         return None
 
@@ -18,8 +29,8 @@ def createMatrixFromParameters(coefs:list, gates:list) -> np.array:
     matrix = np.zeros((2**len(gates[0]), 2**len(gates[0])), dtype=complex)
     for gate in gates:
         tempGate = None
-        gate = gate[::-1]
-        for letter in gate:
+        gateTemp = gate[::-1]
+        for letter in gateTemp:
             if tempGate is None:
                 tempGate = letterToGate(letter)
             else:
