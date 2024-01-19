502e0c981e77b7078244a88bdefc1e20108e2887
Pijus <pijus.petkevicius@bentley.com>
2024-01-19 11:13:38 +0200

    improved norm estimation

--- VQLS-QSVM-Implementation/Code/VQLS/NormEstimation.py
+++ VQLS-QSVM-Implementation/Code/VQLS/NormEstimation.py
@@ -1,31 +1,28 @@
 from typing import List
 import numpy as np
-from itertools import product
 
 # Quantum normalized vector after getSolutionVector can have negative or positive values,
-# so we need to check all combinations of signs, which one returns the minimum difference between b and bEstimated
-# minimum difference between b and bEstimated is the sign combination we are looking for
+# so we need to check combinations of signs, which one returns the minimum difference between b and bEstimated
+# we do it by multiplying by -1 each element of the vector and checking the difference, if it is smaller, we keep the sign of i-th element
+# we do it for all elements of the vector
 def bestMatchingSignsVector(
     A: np.ndarray, xEstimated: np.array, b: np.array
 ) -> List[float]:
-    values: List[int] = [-1, 1]
-    combos: List[float] = list(
-        product(values, repeat=len(xEstimated)) # this has to be rethought, since 2^2^5 is too much
-    )  # generates all 8 bit combinations
-    minDifference: float = 10000000
-    minDifferenceValue: List[float] = []
-    for combo in combos:
-        vc: List[float] = np.multiply(
-            xEstimated, list(combo)
-        )  # multiply each element of vector with the corresponding element of combo
-        bEstimated: List[float] = A.dot(vc)  # calculate bEst
-        difference: float = np.linalg.norm(
-            bEstimated - b
-        )  # calculate difference between b and bEstimated
-        if difference < minDifference:
+    xEstimated = np.array(xEstimated)
+    minDifference: float = calculateDifference(A, xEstimated, b)
+    for i in range(len(xEstimated)):
+        xEstimated[i] = -1 * xEstimated[i]
+        difference = calculateDifference(A, xEstimated, b)
+        if minDifference < difference:
+            xEstimated[i] = -1 * xEstimated[i]
+        else:
             minDifference = difference
-            minDifferenceValue = vc
-    return minDifferenceValue
+    return xEstimated
+
+
+def calculateDifference(A: np.ndarray, xEstimated: np.array, b: np.array) -> float:
+    bEstimated: List[float] = A.dot(xEstimated)  # calculate bEstimated
+    return np.linalg.norm(bEstimated - b)
 
 
 # estimate norm of vector
@@ -36,6 +33,10 @@ def estimateNorm(
     A: np.ndarray, estimatedX: np.array, b: np.array, verbose: bool = False
 ) -> (float, List[float]):
     v: List[float] = bestMatchingSignsVector(A, estimatedX, b)
+    # rightSide1: float = np.linalg.norm(b)
+    # leftSide1: float = np.linalg.norm(A.dot(v))
+    # estimatedNorm2: float = rightSide1 / leftSide1
+     
     leftSide: float = b.T.dot(A.dot(v))
     rightSide: float = b.T.dot(b)  # maybe test this with \vec{1} vector
     estimatedNorm: float = rightSide / leftSide
